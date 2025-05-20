07516ed4f0bb114a5540c6e17762eb3e69a385da
Harshal <harshal.bhosale@techalchemy.com>
2025-05-20 15:56:22 +0530

    latest version of data

--- mycode.py
+++ mycode.py
@@ -8,6 +8,12 @@ data = {'Name': ['Alice', 'Bob', 'sam'],
 
 df = pd.DataFrame(data)
 
+new_data = {'Name': 'rahul', 'age':20, 'city':'pune'}
+df.loc[len(df.index)] = new_data
+
+new_data_1 = {'Name': 'sakshi', 'age':25, 'city':'pune'}
+df.loc[len(df.index)] = new_data_1
+
 data_dir = "data"
 os.makedirs(data_dir, exist_ok=True)
 
